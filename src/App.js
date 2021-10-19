bc61ca4ca7fe66c3a7b7f4e7a5605072669fc145
Sruthi Raj <sruthiraj97sruthizzzz@gmail.com>
2021-10-19 15:56:28 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import groupArrays from './components/ArrayData';
 import ArrayData from './components/ArrayData';
 
 function App() {
   return (
     <div className="App">
-      <ArrayData/>
-      
+    <ArrayData/>
     </div>
   );
 }
